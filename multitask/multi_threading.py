d46fe3679f5065a4f785cfea66001a18a0110c46
wisedeve <18612124664@163.com>
2017-06-15 16:20:43 +0800

    正则表达式

--- multitask/multi_threading.py
+++ multitask/multi_threading.py
@@ -1,5 +1,9 @@
 import time, threading
 
+# 由于线程是操作系统直接支持的执行单元，因此，高级语言通常都内置多线程的支持，
+# Python也不例外，并且，Python的线程是真正的Posix Thread，而不是模拟出来的线程。
+# 启动一个线程就是把一个函数传入并创建Thread实例，然后调用start()开始执行：
+
 # 新线程执行的方法
 def loop():
     print('thread %s is running... '% threading.current_thread().name)
